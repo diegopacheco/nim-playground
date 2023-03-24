b2114c6eed6dd4d92ff45a86439a1ce809268479
diegopacheco <diego.pacheco.it@gmail.com>
2023-03-24 02:13:35 -0700

    loop

--- loops-fun/src/main.nim
+++ loops-fun/src/main.nim
@@ -1,2 +1,23 @@
+import strformat
+
 for i in 1 .. 10:
     echo i
+
+for n in countdown(10, 5):
+    echo fmt"counting down: {n}"
+
+let word = "nim_programing_lang"
+for c in word:
+    echo fmt"char is: {c}"
+
+# immutable because of let
+let word2 = "cool"
+for i, c in word2:
+    echo fmt"{i} index char is: {c}"
+
+# mmutable because of let
+var x = 0
+while x<10:
+    inc x
+    if x > 8:
+        echo x
